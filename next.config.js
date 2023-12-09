c58a63436cb7185f227f8b9cbac39c89845ba943
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2023-12-09 01:25:00 +0100

    Init

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    env: {
-        NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "dk30psegu",
-        NEXT_PUBLIC_CLOUDINARY_PRESET_NAME: "hkhpkcfr"
-    },
     images: {
         remotePatterns: [
             {
